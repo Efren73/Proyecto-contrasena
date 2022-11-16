62fa451a2dd0eaf807fe1ccc086ac2756fe74114
Efren73 <A01276266@itesm.mx>
2022-11-15 23:31:38 -0600

    Dando diseno a proyecto de password

--- src/contrasena-code.jsx
+++ src/contrasena-code.jsx
@@ -10,6 +10,7 @@ function Contrasena() {
     const [minusculas, setMinusculas] = useState(0)
     const [simbolos, setSimbolos] = useState(0)
     const [caracteres, setCaracteres] = useState(0)
+    const [contraSegura, setContraSegura] = useState(false)
 
 
     function handleChange(e){
@@ -33,6 +34,7 @@ function Contrasena() {
         setMayusculas(false)
         setMinusculas(false)
         setSimbolos(false)
+        setContraSegura(false)
 
         var cont = 0
         if(contrasena.length >= 10){
@@ -66,37 +68,49 @@ function Contrasena() {
         }
 
         if(cont === 6){
-            window.alert("Tu contraseña es segura")
+            setContraSegura(true)
         }
     }
 
 
 
   return (
-    <div>
-        <label>
-            Contraseña
-            <input type="password" value={contrasena} onChange={handleChange}/>
-        </label>
-        <input type="button" onClick={Validar} value="Validar"/>
-        {
-            longitud === false ? <p>La longitud minima debe ser de 10 caracteres</p> : <div></div>
-        }
-        {
-            numeros === false ? <p>La contraseña debe tener al menos un numero</p> : <div></div>
-        }
-        {
-            mayusculas === false ? <p>La contrasena debe tener al menos una mayuscula</p>: <div> </div>
-        }
-        {
-            minusculas=== false ? <p>La contrasena debe tener al menos una minuscula</p>: <div> </div>
-        }
-        {
-            simbolos === false ? <p>La contrasena debe tener al menos un símbolo como !$%&?</p>: <div></div>
-        }
-        {
-            caracteres === false ? <p>La contrasena debe tener al menos un caracter especial como [],(),/\</p> : <div></div>
-        }
+    <div className='formulario'>
+        <img src="https://www.cuatromedios.com/media/5smjq5S3yV/original/logo-akky-transparente-320-180.png" />
+
+        <div>
+            <label>
+                Contraseña
+                <input type="password" id='claveActual' value={contrasena} onChange={handleChange}/>
+            </label>
+            <input type="button" onClick={Validar} value="Validar"/>
+            {
+                longitud === false ? <p>La longitud minima debe ser de 10 caracteres</p> : <div></div>
+            }
+            {
+                numeros === false ? <p>La contraseña debe tener al menos un numero</p> : <div></div>
+            }
+            {
+                mayusculas === false ? <p>La contrasena debe tener al menos una mayuscula</p>: <div> </div>
+            }
+            {
+                minusculas=== false ? <p>La contrasena debe tener al menos una minuscula</p>: <div> </div>
+            }
+            {
+                simbolos === false ? <p>La contrasena debe tener al menos un símbolo como !$%&?</p>: <div></div>
+            }
+            {
+                caracteres === false ? <p>La contrasena debe tener al menos un caracter especial como [],(),/\</p> : <div></div>
+            }
+            {
+                contraSegura === true ? <div>
+                    <center>
+                        <p className='Segura'>¡FELICIDADES! </p>
+                        <p className='Segura'>Tu contraseña es segura</p>
+                    </center>
+                    </div> : <div></div>
+            }
+        </div>
     </div>
   );
 }
